2edb6241b8004c48546e7d2268c3e1f018a16b63
Dival RF <divalrmd12@gmail.com>
2021-09-18 09:46:29 +0700

    menambahkan dan membuat nilai default dari setiap objek yang di instansiasi

---
+++ produk.php
@@ -0,0 +1,11 @@
+<?php
+
+class Produk {
+    public $judul = "judul",
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0;
+}
+
+$produk1 = new Produk();
+var_dump($produk1);
