6d799a181c238fe430620113d614da3caae8866b
Dival RF <divalrmd12@gmail.com>
2021-09-18 10:08:57 +0700

    menuliskan property pada produk 3 tentang penulis dan penerbit

--- produk.php
+++ produk.php
@@ -7,11 +7,19 @@ class Produk {
            $harga = 0;
 }
 
-$produk1 = new Produk();
-$produk1->judul = "Naruto";
-var_dump($produk1);
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
 
-$produk2 = new produk();
-$produk2->judul = "uncharted";
-$produk2->tambahProperty = "hahaha";
-var_dump($produk2);
+// $produk2 = new produk();
+// $produk2->judul = "uncharted";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2);
+
+$produk3 = new produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+echo "Komik : $produk3->penulis, $produk3->penerbit";
